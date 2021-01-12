4dfd956bcdf55ed2ffa6d4b1790d35a64a25c170
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2021-01-12 15:04:01 -0700

    Final commit.

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -5,5 +5,15 @@ class FoodTruck
   def initialize(name)
     @name = name
     @inventory = {}
+    @truck_stock = 0
+  end
+
+  def check_stock(item)
+    @truck_stock
+  end
+
+  def stock(item, quantity)
+    inventory[item] = quantity
+    @truck_stock += quantity
   end
 end
