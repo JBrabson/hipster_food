8ff66b8f168a30f37960da21aa56e62b7ef43222
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2021-01-12 13:41:37 -0700

    Add inventory method and test; passing.

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -11,4 +11,9 @@ class FoodTruckTest < Minitest::Test
     assert_instance_of FoodTruck, food_truck
     assert_equal "Rocky Mountain Pies", food_truck.name
   end
+
+  def test_it_starts_with_no_inventory
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    assert_equal ({}), food_truck.inventory
+  end
 end
